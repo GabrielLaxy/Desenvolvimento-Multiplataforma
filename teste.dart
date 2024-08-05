0b286aaceb195b25e175a95271b76d6b4745fa4e
GabrielLaxy <gablaxycarreira@gmail.com>
2024-08-05 09:22:10 -0300

    primeiro codigo em dart

---
+++ teste.dart
@@ -0,0 +1,45 @@
+void main() {
+  String idadeTextual = "35";
+  int idade = int.parse(idadeTextual);
+  print(idade);
+
+  String pesoAtual = "80.5";
+  double peso = double.parse(pesoAtual);
+  print(peso);
+
+  String alturaTextual = "1.8";
+  num altura = num.parse(alturaTextual);
+  print(altura);
+  print(altura.runtimeType);
+
+  // String letra = "a";
+  // print(letra * 10);
+  // String nome = "João";
+  // int idade = 20;
+  // print("Meu nome é" + nome);
+  // print("tenho " + idade.toString() + "anos");
+
+  // print("Meu nome é $nome");
+  // print("Ano que vem farei ${idade + 1} anos");
+
+  // int a = 2;
+  // print(a);
+  // print(a.runtimeType);
+
+  // double d = 2.5;
+  // print(d);
+  // print(d.runtimeType);
+
+  // double d2 = 2;
+  // print(d2);
+  // print(d.runtimeType);
+
+  // num n1 = 1;
+  // num n2 = 1.1;
+
+  // print(n1.runtimeType);
+  // print(n2.runtimeType);
+
+  // String teste = "pula linha: \n, teste";
+  // print(teste);
+}
